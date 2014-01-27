9c48a920fc0d53497625139f80485e5a825ad4d7
Sergey Panfilov <sergray@gmail.com>
2014-01-27 18:10:45 +0200

    Fix test script

--- bin/vimeo-test.py
+++ bin/vimeo-test.py
@@ -23,9 +23,8 @@ This script contains various call to vimeo API
 in order the check the correct behavior of python-vimeo.
 """
 
-from vimeo import SimpleOAuthClient
+from vimeo import VimeoClient
 import vimeo
-import oauth.oauth as oauth
 import sys
 import optparse
 
@@ -70,45 +69,41 @@ def test_videos(client):
     user_ids=[xxx, yyy, zzz]
     video_ids=[xxx, yyy, zzz]
     client.vimeo_videos_addCast(user_ids[0], video_ids[0])
-
     client.vimeo_videos_addPhotos(photos_urls, video_id)
     client.vimeo_videos_addTags(tags, video_id)
     client.vimeo_videos_clearTags(video_id)
     client.vimeo_videos_delete(video_id)
-    client.vimeo_videos_getAll(user_id,)
-    client.vimeo_videos_getAppearsIn(user_id,)
-    client.vimeo_videos_getByTag(tag, )
-    client.vimeo_videos_getCast(video_id,)
-    client.vimeo_videos_getContactsLiked(user_id,)
-    client.vimeo_videos_getContactsUploaded(user_id, )
+    client.vimeo_videos_getAll(user_id)
+    client.vimeo_videos_getAppearsIn(user_id)
+    client.vimeo_videos_getByTag(tag)
+    client.vimeo_videos_getCast(video_id)
+    client.vimeo_videos_getContactsLiked(user_id)
+    client.vimeo_videos_getContactsUploaded(user_id)
     client.vimeo_videos_getInfo(video_id)
-    client.vimeo_videos_getLikes(user_id,)
+    client.vimeo_videos_getLikes(user_id)
     client.vimeo_videos_getSourceFileUrls(video_id)
-    client.vimeo_videos_getSubscriptions(user_id,
+    client.vimeo_videos_getSubscriptions(user_id)
     client.vimeo_videos_getThumbnailUrls(video_id)
-    client.vimeo_videos_getUploaded(user_id,)
+    client.vimeo_videos_getUploaded(user_id)
     client.vimeo_videos_removeCast(user_id, video_id)
     client.vimeo_videos_removeTag(tag_id, video_id)
-    client.vimeo_videos_search(query, user_id=None,)
+    client.vimeo_videos_search(query, user_id=None)
     client.vimeo_videos_setDescription(description, video_id)
     client.vimeo_videos_setLike(like, video_id)
-    client.vimeo_videos_setPrivacy(privacy, video_id,)
+    client.vimeo_videos_setPrivacy(privacy, video_id)
     client.vimeo_videos_setTitle(title, video_id)
-    client.vimeo_videos_comments_addComment(comment_text, video_id,)
+    client.vimeo_videos_comments_addComment(comment_text, video_id)
     client.vimeo_videos_comments_deleteComment(comment_id, video_id)
-    client.vimeo_videos_comments_editComment(comment_id,)
-    client.vimeo_videos_comments_getList(video_id,)
+    client.vimeo_videos_comments_editComment(comment_id)
+    client.vimeo_videos_comments_getList(video_id)
     client.vimeo_videos_embed_getPresets(page=None, per_page=None)
-    client.vimeo_videos_embed_setPreset(preset_id,)
+    client.vimeo_videos_embed_setPreset(preset_id)
 
-
-def test_upload(client):
-                                             
+# def test_upload(client):                                           
 #     client.vimeo_videos_upload_checkTicket(ticket_id)
 #     client.vimeo_videos_upload_confirm(ticket_id,
 #     client.vimeo_videos_upload_getTicket(self)
 #     client.vimeo_videos_upload_verifyManifest(json_manifest, ticket_id, xml_manifest)
-
 
 
 def main(argv):
@@ -132,14 +127,14 @@ def main(argv):
         sys.exit(-1)
 
     if None in (options.access_token, options.access_token_secret):
-        client = SimpleOAuthClient(options.key, options.secret)
+        client = VimeoClient(options.key, options.secret)
         client.get_request_token()
         print client.get_authorize_token_url()
         verifier = sys.stdin.readline().strip()
         print "Using ", verifier, " as verifier"
         print "Token is:", client.get_access_token(verifier)
     else:
-        client = SimpleOAuthClient(options.key, options.secret,
+        client = VimeoClient(options.key, options.secret,
                                    token=options.access_token,
                                    token_secret=options.access_token_secret)
 
@@ -176,5 +171,3 @@ def main(argv):
 if __name__ == '__main__':
     main(sys.argv)
     ##print vimeo.user_videos('dkm')
-
-
